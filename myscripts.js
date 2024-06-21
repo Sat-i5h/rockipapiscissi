6c76d25cf3b128510c559413c83c28a6df9fd090
Sat-i5h <satishgogula96@gmail.com>
2024-06-21 20:24:03 +0530

    added 5 rounds and who won

--- myscripts.js
+++ myscripts.js
@@ -1,5 +1,6 @@
 let humanScore=0;
 let computerScore=0;
+let roundNumber=0
 
 function getComputerChoice(){
  randomNumber= Math.floor(Math.random()*3)
@@ -16,51 +17,67 @@ function getComputerChoice(){
     return "rock";
  }
 } 
-//console.log(getComputerChoice());
 
-function getHumanChoice(){
-    let humanPrompt=prompt("Please enter either Rock, Paper or Scissiors");
-    humanChoice=humanPrompt.toLowerCase()
-    //console.log(humanChoice);
-    return humanChoice;
-}
+
+
+const rock=document.querySelector("#btn_rock")
+rock.addEventListener("click", ()=>{
+   let humanChoice="rock";
+   playRound(humanChoice,getComputerChoice());
+});
+
+const paper=document.querySelector("#btn_paper")
+paper.addEventListener("click", ()=>{
+   let humanChoice="paper";
+   playRound(humanChoice,getComputerChoice());
+});
+
+const scissors=document.querySelector("#btn_scissors")
+scissors.addEventListener("click", ()=>{
+   let humanChoice="scissors";
+   playRound(humanChoice,getComputerChoice());
+});
+
+const round=document.querySelector("#round");
+const pScore=document.querySelector("#pScore");
+const cScore=document.querySelector("#cScore");
+
+
 
 
 function playRound(humanChoice,computerChoice){
- let hChoice=getHumanChoice()
  let cChoice=getComputerChoice()
- if (hChoice=='rock' || hChoice=='paper' || hChoice=='scissors'){
- if (hChoice==cChoice){
-    console.log("It's a draw!")
-    console.log(cChoice)
+ if(roundNumber<5){
+ if (humanChoice=='rock' || humanChoice=='paper' || humanChoice=='scissors'){
+ if (humanChoice==cChoice){
+    roundNumber++;
+    round.textContent=`RoundNumber: ${roundNumber}`;
  }
- else if(hChoice=="rock" && cChoice=="paper"){
-    console.log("Computer wins!")
-    console.log(cChoice)
+ else if(humanChoice=="rock" && cChoice=="paper"){
     computerScore++
-    console.log("computer score "+ computerScore)
+    roundNumber++;
+    round.textContent=`RoundNumber: ${roundNumber}`;
+    cScore.textContent=`ComputerScore: ${computerScore}`;
  }
  else{
-    console.log("Human wins!")
-    console.log(cChoice)
     humanScore++
-    console.log("human score "+ humanScore)
+    roundNumber++;
+    round.textContent=`RoundNumber: ${roundNumber}`;
+    pScore.textContent=`PlayerScore: ${humanScore}`;
  }
 }
-else{
-    console.log("PLEASE ENTER VALID INPUT")
+}
+else if(roundNumber==5){
+   round.textContent=`RoundNumber Reached ${roundNumber} GAME OVER!!!`;
+   if(humanScore>computerScore){
+      pScore.textContent=`PlayerScore Won with Score ${humanScore}`;
+   }
+   else if(computerScore>humanScore){
+      cScore.textContent=`Computer Won with Score ${computerScore}`;
+   }
+   else{
+      pScore.textContent=`It's a Draw!!!!`
+      cScore.textContent="It's a Draw!!!!"
+   }
 }
 }
-
-
-function playGame(){
-
-    playRound()
-    playRound()
-    playRound()
-    playRound()
-    playRound()
-    console.log("final human score= "+humanScore)
-    console.log("final computer score= "+computerScore)
-}
-playGame()
